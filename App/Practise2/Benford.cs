242c122c085f683464f174b2f4221d9ad5168270
champion <n.laptev1@g.nsu.ru>
2025-04-15 18:26:58 +0700

    benfors without tests

--- App/Practise2/Benford.cs
+++ App/Practise2/Benford.cs
@@ -7,9 +7,15 @@ public class Benford
     static int[] GetBenfordStatistics(string text)
     {
         var statistics = new int[10];
-	
-        // code here
-	
+        Array.Fill(statistics, 0);
+        string[] message = text.Split(' ');
+        for (int i = 0; i < message.Length; i++)
+        {
+            if (message[i].All(char.IsDigit) && message[i][0] != '0')
+            {
+                statistics[message[i][0] - '0']++;
+            }
+        }
         return statistics;
     }
 }
