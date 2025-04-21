3424628417ceac95451f4199728ff2438a9b61b6
champion <n.laptev1@g.nsu.ru>
2025-04-21 21:39:20 +0700

    tasks 1, 2 and 3

--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,9 @@
 namespace App;
-using App.Practise2;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello, world!");
     }
 }
---
+++ AppTests/Practise2/BenfordTest.cs
@@ -0,0 +1,23 @@
+using App.Practise2;
+
+namespace AppTests.Practise2;
+
+[TestFixture]
+[TestOf(typeof(Benford))]
+public class BenfordTest
+{
+
+    [Test]
+    [TestCase("0 1 2 3 4 5 6 7 8 9", new int[] {0,1,1,1,1,1,1,1,1,1})]
+    [TestCase("0 01 02 03 04 05 06 07 08 09", new int[] {0,0,0,0,0,0,0,0,0,0})]
+    [TestCase("Price: 1234. And my level of English is B2", new int[] {0,1,0,0,0,0,0,0,0,0})]
+    [TestCase("N0 5nu5mbe4rs here12", new int[] {0,0,0,0,0,0,0,0,0,0})]
+    [TestCase("100 101 102 200 201 300", new int[] {0,3,2,1,0,0,0,0,0,0})]
+
+    public void TestingBenfordMethod(string text, int[] expected)
+    {
+        var array = Benford.GetBenfordStatistics(text);
+        
+        Assert.AreEqual(expected, array);
+    }
+}
