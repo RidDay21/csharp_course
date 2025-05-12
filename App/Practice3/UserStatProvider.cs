1b8562f54aa1d38f81b7ae2c0766225accfa0c62
champion <n.laptev1@g.nsu.ru>
2025-05-12 18:20:55 +0700

    Декомпозировал код и добавил нормальных логов

--- App/Practice3/UserStatProvider.cs
+++ App/Practice3/UserStatProvider.cs
@@ -1,210 +1,163 @@
-using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
-namespace App.Practice3;
-
-/**
- * Что надо сделать?
- *  Мы должны все эти записи в логах агрегировать по месяцам
- * (или по дням, в зависимости от запроса) и положить в один словарик в классе `UserActionStatItem`.
- */
-public class UserStatProvider
+namespace App.Practice3
 {
-    private UserActionStatResponse _suitableActionsPerDay;
-    private UserActionStatResponse _suitableActionsPerMonth;
-
-    public UserStatProvider()
+    public class UserStatProvider
     {
-        _suitableActionsPerDay = new UserActionStatResponse()
+        public UserActionStatResponse GetUserActionStat(UserActionStatRequest request,
+            List<UserActionItem> userActionItems)
         {
-            UserActionStat = new List<UserActionStatItem>()
-        };
-        _suitableActionsPerMonth = new UserActionStatResponse()
-        {
-            UserActionStat = new List<UserActionStatItem>()
-        };
-        Console.WriteLine("[UserStatProvider] Constructor was called.");
-        
-    }
+            Logging("Requested user action statistics.");
+            Logging($"Grouping type: {request.DateGroupType}, Start: {request.StartDate:dd/MM/yyyy}, End: {request.EndDate:dd/MM/yyyy}");
 
-    public UserActionStatResponse GetUserActionStat(UserActionStatRequest request,
-        List<UserActionItem> userActionItems)
-    {
-        if (userActionItems == null || userActionItems.Count == 0)
-        {
-            Console.WriteLine("Исключения");
-            return null;
-        }
-        switch (request.DateGroupType)
-        {
-            case DateGroupTypes.Daily:
-                Console.WriteLine("[GetUserActionStat] Daily Ahalitics were called. Cur terms are:\n" +
-                                  $"\tStart: {request.StartDate:dd/MM/yyyy}\n" +
-                                  $"\tEnd: {request.EndDate:dd/MM/yyyy}\n" +
-                                  $"\tMetrics: {request.DateGroupType}");
-                GroupByDays(request, userActionItems);
-                return _suitableActionsPerDay;
-            case DateGroupTypes.Monthly:
-                Console.WriteLine("[GetUserActionStat] Monthly Analitics were called. Cur terms are:\n" +
-                                  $"\tStart: {request.StartDate:dd/MM/yyyy}\n" +
-                                  $"\tEnd: {request.EndDate:dd/MM/yyyy}\n" +
-                                  $"\tMetrics: {request.DateGroupType}");
-                GroupByMonths(request, userActionItems);
-                return _suitableActionsPerMonth;
-            default:
-                throw new AbandonedMutexException();
-        }
-    }
-
-    void GroupByDays(UserActionStatRequest request, List<UserActionItem> userActionItems)
-    {
-        var strDate = request.StartDate;
-        var enDate = request.EndDate;
-        foreach (var userActionItem in userActionItems)
-        {
-            Console.WriteLine("[GroupByDays] Start passing throw userActionItems\n" +
-                              $"Cur Element:\n\tName: \"{userActionItem.Action}\"\n\tDate: {userActionItem.Date}\n");
-            if (userActionItem.Date >= strDate)
+            UserActionStatResponse suitableActions = new UserActionStatResponse
             {
-                Console.WriteLine($"[GroupByDays] Daily Analitics were called, because {userActionItem.Date} is after or equal to {strDate.Date}.");
-                AddItemToSuitActionsPerDay(userActionItem);
+                UserActionStat = new List<UserActionStatItem>()
+            };
+
+            if (userActionItems == null || userActionItems.Count == 0)
+            {
+                Logging("UserActionItems is null or empty. No data to process.");
+                return null;
             }
-            else if (userActionItem.Date >= enDate)
-            {
-                break;
-            }
-        }
-    }
 
-    void AddItemToSuitActionsPerDay(UserActionItem item)
-    {
-        Console.WriteLine($"[AddItemToSuitActionsPerDay] Cur item to add: {item.Action}");
-        bool isEmpty = _suitableActionsPerDay.UserActionStat.Count == 0;
-        if (isEmpty)
-        {
-            var dict = new  Dictionary<ActionTypes, int>();
-            dict.Add(item.Action, item.Count);
-            Console.WriteLine($"[AddItemToSuitActionsPerDay]Create new Dictionary for " +
-                              $"ActionMetrics: {dict[item.Action]}.");
-            
-            var userActionStatItem = new UserActionStatItem();
-            userActionStatItem.StartDate = item.Date;
-            userActionStatItem.EndDate = item.Date;
-            userActionStatItem.ActionMetrics = dict;
-            _suitableActionsPerDay.UserActionStat.Add(userActionStatItem);
-            Console.WriteLine($"[AddItemToSuitActionsPerDay]Add new pair to " +
-                              $"ActionMetrics: {dict[item.Action]}.");
-        }
-        else if (_suitableActionsPerDay.UserActionStat.Last().StartDate == item.Date)
-        {
-            Console.WriteLine("[AddItemToSuitActionsPerDay] Item's day has already " +
-                              $"exist in _suitableActionPerDay.{item.Action}");
-            _suitableActionsPerDay.UserActionStat.Last().ActionMetrics.Add(item.Action, item.Count);
-            Console.WriteLine($"[AddItemToSuitActionsPerDay] Item{item.Action} has been succesfully" +
-                              $"added to dictionary.");
-        } else
-        {
-            Console.WriteLine("[AddItemToSuitActionsPerDay] Item's day absents" +
-                              $"in _suitableActionPerDay.(Item: {item.Action})");
-            var dict = new  Dictionary<ActionTypes, int>();
-            dict.Add(item.Action, item.Count);
-            Console.WriteLine($"[AddItemToSuitActionsPerDay]Create new Dictionary for " +
-                              $"ActionMetrics: {dict[item.Action]}.");
-            
-            var userActionStatItem = new UserActionStatItem();
-            userActionStatItem.StartDate = item.Date;
-            userActionStatItem.EndDate = item.Date;
-            userActionStatItem.ActionMetrics = dict;
-            _suitableActionsPerDay.UserActionStat.Add(userActionStatItem);
-            Console.WriteLine($"[AddItemToSuitActionsPerDay]Add new pair to " +
-                              $"ActionMetrics: {dict[item.Action]}.");
-        }
-    }
-    
-    
-
-    void GroupByMonths(UserActionStatRequest request, List<UserActionItem> userActionItems)
-    {
-        var strDate = request.StartDate;
-        var enDate = request.EndDate;
-        Console.WriteLine("[GroupByMonths] Start passing throw userActionItems\n");
-        foreach (var userActionItem in userActionItems)
-        {
-            if (userActionItem.Date >= strDate && userActionItem.Date <= enDate)
+            switch (request.DateGroupType)
             {
-                Console.WriteLine($"[GroupByMonths] Start to adding Item:\n\t{userActionItem.Action}");
-                AddItemToSuitActionsPerMonth(userActionItem, strDate, enDate);
+                case DateGroupTypes.Daily:
+                    Logging("Processing daily statistics.");
+                    GroupItemsByDays(request, userActionItems, ref suitableActions);
+                    break;
+                case DateGroupTypes.Monthly:
+                    Logging("Processing monthly statistics.");
+                    GroupItemsByMonths(request, userActionItems, ref suitableActions);
+                    break;
+                default:
+                    Logging($"Unknown DateGroupType: {request.DateGroupType}. Aborting.");
+                    throw new AbandonedMutexException("Unknown date group type");
             }
-            else if (userActionItem.Date >= enDate)
+
+            return suitableActions;
+        }
+
+        void GroupItemsByMonths(UserActionStatRequest request, List<UserActionItem> userActionItems,
+            ref UserActionStatResponse suitableActions)
+        {
+            Logging("Grouping items by months.");
+            GroupItemsByPeriod(request, userActionItems, ref suitableActions, ShouldCreateNewMontlyItem);
+        }
+
+        void GroupItemsByDays(UserActionStatRequest request, List<UserActionItem> userActionItems,
+            ref UserActionStatResponse suitableActions)
+        {
+            Logging("Grouping items by days.");
+            GroupItemsByPeriod(request, userActionItems, ref suitableActions, ShouldCreateNewDailyItem);
+        }
+
+        bool IsDateInRange(UserActionStatRequest request, DateTime date) =>
+            (date.Date >= request.StartDate && date.Date <= request.EndDate);
+
+        void GroupItemsByPeriod(UserActionStatRequest request, List<UserActionItem> userActionItems,
+            ref UserActionStatResponse suitableActions,
+            Func<UserActionStatResponse, DateTime, bool> termForCreateNewItemInResponse)
+        {
+            Console.WriteLine("[GroupItemsByPeriod] Processing user actions within date range...");
+
+            foreach (var userActionItem in userActionItems)
             {
-                break;
+                if (!IsDateInRange(request, userActionItem.Date))
+                {
+                    Console.WriteLine($"[GroupItemsByPeriod] Skipping action outside date range: {userActionItem.Date:dd/MM/yyyy}");
+                    continue;
+                }
+
+                Console.WriteLine($"[GroupItemsByPeriod] Handling action: {userActionItem.Action} on {userActionItem.Date:dd/MM/yyyy}");
+                AddItemToSuitActions(userActionItem, request, suitableActions, termForCreateNewItemInResponse);
             }
         }
-    }
 
-    void AddItemToSuitActionsPerMonth(UserActionItem item, DateTime startDate, DateTime endDate)
-    {
-        Console.WriteLine($"[AddItemToSuitActionsPerMonth] Cur item to add: {item.Action}");
-        var date = item.Date;
-        if (_suitableActionsPerMonth.UserActionStat.Count == 0)
+        bool TermForMontlyRequest(UserActionStatResponse response, DateTime date) =>
+            (response.UserActionStat.Last().StartDate.Month != date.Month);
+
+        bool TermForDailyRequest(UserActionStatResponse response, DateTime date) =>
+            (response.UserActionStat.Last().StartDate.Day != date.Day);
+
+        bool ShouldCreateNewDailyItem(UserActionStatResponse suitableActions, DateTime date)
         {
-            Console.WriteLine($"[AddItemToSuitActionsPerMonth] Cur array is empty.Item: {item.Action}");
-            var dict = new Dictionary<ActionTypes, int>();
-            dict.Add(item.Action, item.Count);
-            var end = new DateTime(
-                date.Year,
-                date.Month,
-                DateTime.DaysInMonth(date.Year, date.Month));
-            end = end < endDate ? end : endDate;
+            Console.WriteLine($"[ShouldCreateNewDailyItem] Checking for new daily group on {date:dd/MM/yyyy}");
+            return suitableActions.UserActionStat.Count == 0 || TermForDailyRequest(suitableActions, date);
+        }
 
-            int day;
-            if (startDate.Month == date.Month)
-                day = startDate.Day;
+        bool ShouldCreateNewMontlyItem(UserActionStatResponse suitableActions, DateTime date)
+        {
+            Console.WriteLine($"[ShouldCreateNewMontlyItem] Checking for new monthly group on {date:MM/yyyy}");
+            return suitableActions.UserActionStat.Count == 0 || TermForMontlyRequest(suitableActions, date);
+        }
+
+        void AddItemToSuitActions(UserActionItem item, UserActionStatRequest request, UserActionStatResponse suitableActions,
+            Func<UserActionStatResponse, DateTime, bool> termForCreateNewItemInResponse)
+        {
+            Console.WriteLine($"[AddItemToSuitActions] Preparing to add action: {item.Action}, Count: {item.Count}, Date: {item.Date:dd/MM/yyyy}");
+
+            if (termForCreateNewItemInResponse(suitableActions, item.Date))
+            {
+                var dict = new Dictionary<ActionTypes, int> { { item.Action, item.Count } };
+                Console.WriteLine($"[AddItemToSuitActions] New date range detected. Creating new action metrics entry for {item.Date:dd/MM/yyyy}.");
+
+                DateTime start, end;
+                if (request.DateGroupType == DateGroupTypes.Monthly)
+                {
+                    start = DateCalculator.GetStartDay(item.Date, request.StartDate);
+                    end = DateCalculator.GetEndDay(item.Date, request.EndDate);
+                }
+                else
+                {
+                    start = end = item.Date;
+                }
+
+                var userActionStatItem = new UserActionStatItem
+                {
+                    StartDate = start,
+                    EndDate = end,
+                    ActionMetrics = dict
+                };
+
+                suitableActions.UserActionStat.Add(userActionStatItem);
+                Console.WriteLine($"[AddItemToSuitActions] Action {item.Action} with count {item.Count} added to new date group.");
+            }
             else
-                day = 1;
-            var start = new DateTime(
-                date.Year,
-                date.Month,
-                day);
-            var userActionStatItem = new UserActionStatItem();
-            userActionStatItem.StartDate = start;
-            userActionStatItem.EndDate = end;
-            userActionStatItem.ActionMetrics = dict;
-            _suitableActionsPerMonth.UserActionStat.Add(userActionStatItem);
-            Console.WriteLine($"[AddItemToSuitActionsPerMonth] New Montly Array created.Item: {item.Action} has been added to dictionary.");
-        } 
-        else if (_suitableActionsPerMonth.UserActionStat.Last().EndDate >= item.Date)
-        {
-            Console.WriteLine($"[AddItemToSuitActionsPerMonth]\n[EndDate >= item.Date] Item: {item.Action} has been added to dictionary.");
-            _suitableActionsPerMonth.UserActionStat.Last().ActionMetrics.Add(
-                item.Action,
-                item.Count);
-        }
-        else 
-        {
-            var dict = new Dictionary<ActionTypes, int>();
-            dict.Add(item.Action, item.Count);
-            var end = new DateTime(
-                date.Year,
-                date.Month,
-                DateTime.DaysInMonth(date.Year, date.Month));
-            end = end < endDate ? end : endDate;
+            {
+                Console.WriteLine($"[AddItemToSuitActions] Existing date group found. Appending action {item.Action} with count {item.Count}.");
+                if (!suitableActions.UserActionStat.Last().ActionMetrics.ContainsKey(item.Action))
+                    suitableActions.UserActionStat.Last().ActionMetrics[item.Action] = item.Count;
+                else
+                    suitableActions.UserActionStat.Last().ActionMetrics[item.Action] += item.Count;
 
-            int day;
-            if (startDate.Month == date.Month)
-                day = startDate.Day;
-            else
-                day = 1;
-            var start = new DateTime(
-                date.Year,
-                date.Month,
-                day);
-            var userActionStatItem = new UserActionStatItem();
-            userActionStatItem.StartDate = start;
-            userActionStatItem.EndDate = end;
-            userActionStatItem.ActionMetrics = dict;
-            _suitableActionsPerMonth.UserActionStat.Add(userActionStatItem);
-            Console.WriteLine($"[AddItemToSuitActionsPerMonth] Item: {item.Action} has been added to dictionary.");
+                Console.WriteLine($"[AddItemToSuitActions] Action {item.Action} successfully added to existing group.");
+            }
         }
-        
+
+        static class DateCalculator
+        {
+            public static DateTime GetStartDay(DateTime date, DateTime startDate)
+            {
+                int day = (startDate.Month == date.Month) ? startDate.Day : 1;
+                return new DateTime(date.Year, date.Month, day);
+            }
+
+            public static DateTime GetEndDay(DateTime date, DateTime endDate)
+            {
+                var end = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+                return end < endDate ? end : endDate;
+            }
+        }
+
+        static void Logging(string message)
+        {
+            string methodName = "[" + new StackTrace().GetFrame(1).GetMethod().Name + "]\t";
+            Console.WriteLine(methodName + message);
+        }
     }
 }
